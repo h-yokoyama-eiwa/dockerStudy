66da73b2967d6162db19a40f953e6f6856d9ad51
h-yokoyama-eiwa <h-yokoyama@esm.co.jp>
2023-10-01 15:42:07 +0900

    カウンターの見た目をCSSで整えた

--- webCounter/web-counter/src/Counter.tsx
+++ webCounter/web-counter/src/Counter.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-// import './App.css';
+import './Counter.css';
 
 function Counter() {
   return (
     <div className="Counter">
-      <p className="Display">
-        0
-      </p>
-      <button className="Button">カウント</button>
-      <button className="Button">リセット</button>
-      <button className="Button">保存</button>
+      <p className='Display'>0</p>
+      <div className='ButtonArea'>
+        <button className="Button">カウント</button>
+        <button className="Button">リセット</button>
+        <button className="Button">保存</button>
+      </div>
     </div>
   );
 }
