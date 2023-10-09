2420753731848b3e4883adae6cea11bab4399af8
h-yokoyama-eiwa <h-yokoyama@esm.co.jp>
2023-10-09 15:26:10 +0900

    ボタンの見た目調整中

--- webCounter/web-counter/src/Counter.tsx
+++ webCounter/web-counter/src/Counter.tsx
@@ -3,12 +3,12 @@ import './Counter.css';
 
 function Counter() {
   return (
-    <div className="Counter">
+    <div className='Counter'>
       <p className='Display'>0</p>
       <div className='ButtonArea'>
-        <button className="Button">カウント</button>
-        <button className="Button">リセット</button>
-        <button className="Button">保存</button>
+        <button className='Button' id='button_count'>カウント</button>
+        <button className='Button' id='button_reset'>リセット</button>
+        <button className='Button' id='button_save'>保存</button>
       </div>
     </div>
   );
