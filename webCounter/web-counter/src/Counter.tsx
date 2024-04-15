a4c2778ce42ebca08011daae102bfa4227c0cafc
h-yokoyama-eiwa <h-yokoyama@esm.co.jp>
2024-04-15 18:48:14 +0900

    見た目を作成した

--- webCounter/web-counter/src/Counter.tsx
+++ webCounter/web-counter/src/Counter.tsx
@@ -1,16 +1,52 @@
 import React from 'react';
 import './Counter.css';
+import Card from '@mui/material/Card';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+import { Spacer } from './Spacer';
+import { StyledCard } from "./styledCard";
+import { StyledTextField } from './styledTextField';
 
 function Counter() {
   return (
-    <div className='Counter'>
-      <p className='Display'>0</p>
-      <div className='ButtonArea'>
-        <button className='CountButton'>カウント</button>
-        <button className='ResetButton'>リセット</button>
-        <button className='SaveButton'>保存</button>
-      </div>
-    </div>
+    <Grid container justifyContent={'center'}>
+      <Grid item xs={6}>
+        <StyledCard>
+          <Card className='card' sx={{ minWidth: 600, maxWidth: 600, height: 300 }}>
+            <Spacer size={30} />
+            <Grid container justifyContent={'center'}>
+              <Grid item xs={10}>
+                <StyledTextField>
+                  <TextField
+                    className='text-field'
+                    variant="outlined"
+                    fullWidth
+                    defaultValue={0}
+                    type='number'
+                    inputProps={{
+                      style: { textAlign: "center"}
+                    }}
+                  />
+                </StyledTextField>
+              </Grid>
+            </Grid>
+            <Spacer size={100} />
+            <Grid container justifyContent={'space-around'} alignItems={'end'}>
+              <Grid item>
+                <Button variant="contained">カウント</Button>
+              </Grid>
+              <Grid item>
+                <Button variant="contained">リセット</Button>
+              </Grid>
+              <Grid item>
+                <Button variant="contained">保存</Button>
+              </Grid>
+            </Grid>
+          </Card>
+        </StyledCard>
+      </Grid>
+    </Grid>
   );
 }
 
