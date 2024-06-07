6f68629d0d2b1fd90ab8336361a0bd94b877a1ec
h-yokoyama-eiwa <h-yokoyama@esm.co.jp>
2024-06-07 17:28:54 +0900

    styled-componentsを使用せずstyleを変更できた

--- webCounter/web-counter/src/Counter.tsx
+++ webCounter/web-counter/src/Counter.tsx
@@ -5,8 +5,6 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
 import { Spacer } from './Spacer';
-import { StyledCard } from "./styledCard";
-import { StyledTextField } from './styledTextField';
 
 function Counter() {
   const [count, setCount] = React.useState(0);
@@ -14,45 +12,52 @@ function Counter() {
   return (
     <Grid container justifyContent={'center'}>
       <Grid item xs={6}>
-        <StyledCard>
-          <Card className='card' sx={{ minWidth: 600, maxWidth: 600, height: 300 }}>
-            <Spacer size={30} />
-            <Grid container justifyContent={'center'}>
-              <Grid item xs={10}>
-                <StyledTextField>
-                  <TextField
-                    className='text-field'
-                    variant="outlined"
-                    fullWidth
-                    defaultValue={count}
-                    type='number'
-                    inputProps={{
-                      style: { textAlign: "center"}
-                    }}
-                  />
-                </StyledTextField>
-              </Grid>
+        <Card
+          className='card'
+          sx={{
+            minWidth: 600,
+            maxWidth: 600,
+            height: 300,
+            backgroundColor: '#ffff00'
+          }}
+        >
+          <Spacer size={30} />
+          <Grid container justifyContent={'center'}>
+            <Grid item xs={10}>
+              <TextField
+                className='text-field'
+                variant="outlined"
+                fullWidth
+                defaultValue={count}
+                type='number'
+                inputProps={{
+                  style: { textAlign: "center"}
+                }}
+                sx={{
+                  backgroundColor: '#ffffff'
+                }}
+              />
             </Grid>
-            <Spacer size={100} />
-            <Grid container justifyContent={'space-around'} alignItems={'end'}>
-              <Grid item>
-                <Button
-                  variant="contained"
-                  onClick={() => setCount(count + 1)}
-                >
-                    カウント
-                </Button>
-              </Grid>
-              <Grid item>
-                <Button variant="contained">リセット</Button>
-              </Grid>
-              <Grid item>
-                <Button variant="contained">保存</Button>
-              </Grid>
+          </Grid>
+          <Spacer size={100} />
+          <Grid container justifyContent={'space-around'} alignItems={'end'}>
+            <Grid item>
+              <Button
+                variant="contained"
+                onClick={() => setCount(count + 1)}
+              >
+                  カウント
+              </Button>
             </Grid>
-          </Card>
-        </StyledCard>
-      </Grid>
+            <Grid item>
+              <Button variant="contained">リセット</Button>
+            </Grid>
+            <Grid item>
+              <Button variant="contained">保存</Button>
+            </Grid>
+          </Grid>
+        </Card>
+    </Grid>
     </Grid>
   );
 }
--- webCounter/web-counter/src/styledTextField.tsx
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components"
-
-export const StyledTextField = styled.div`
-  .text-field {
-    background-color: #ffffff;
-  }
-`;
